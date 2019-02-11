e7bc15b305dcff3fcd7a3cc259aa2b5ac07b63d8
Juan Esteban Henao <jhenaom6@eafit.edu.co>
2019-02-10 19:16:09 -0500

    Listo para subir al docker sin esa configuración solo la app

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/localizacionesbd', {
+mongoose.connect('mongodb://db/localizacionesbd', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
